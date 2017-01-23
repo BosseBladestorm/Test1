f820ca5bb729dd75caf71a7d45f5195d72a4ee92
Robert Svärdström <svardstromr@gmail.com>
2017-01-23 12:56:38 +0100

    Changed two variables value

---
+++ Test/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    int a = 7;
+    int b = 69;
+    int c = 0;
+	// Use this for initialization
+	void Start () {
+        Debug.Log(a);
+        Debug.Log(b);
+        c = a;
+        a = b;
+        b = c;
+        Debug.Log(a);
+        Debug.Log(b);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

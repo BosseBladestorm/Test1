8bb5a67891fd31e45a9cb26e90197431c90c9cb3
Robert Svärdström <svardstromr@gmail.com>
2017-01-30 12:34:56 +0100

    Compared three variables to find the one in the middle

---
+++ Test/Assets/VariableCompare.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableCompare : MonoBehaviour {
+    int a = 1;
+    int b = 69;
+    int c = 5;
+	// Use this for initialization
+	void Start () {
+        if (a > b && a < c || a > c && a < b)
+        {
+            print("a");
+        }
+        else if (b > a && b < c || b < a && b > c)
+        {
+            print("b");
+        }
+        else
+        {
+            print("c");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

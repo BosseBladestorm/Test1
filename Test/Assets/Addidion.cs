3af5b44e1be93bce176621a00803ae6b756f0204
Robert Svärdström <svardstromr@gmail.com>
2017-01-30 11:11:00 +0100

    Compared two variables and printed

---
+++ Test/Assets/Addidion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addidion : MonoBehaviour {
+    int a = 33;
+    int b = 36;
+    int c = 0;
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log (a + b);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Test/Assets/LargestVariable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargestVariable : MonoBehaviour {
+    int a = 4004;
+    int b = 420;
+    
+	// Use this for initialization
+	void Start () {
+        // Checks if a is bigger than b.
+		if (a > b)
+        {
+            // Prints a if a is bigger
+            Debug.Log ("a");
+        }
+        else
+        {
+            //prints b if b is bigger
+            Debug.Log ("b");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
